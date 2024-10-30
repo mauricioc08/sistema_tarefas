fd910bf76e2c9e65f9b5f2f9a7349de302d73ba0
mauricioc08 <mauricio-cassiano@hotmail.com>
2024-10-30 11:33:49 -0300

    add conection fireBase

---
+++ src/services/firebaseConfig.ts
@@ -0,0 +1,21 @@
+// src/services/firebaseConfig.js
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDWqL5H-IkvAmb9aLx3Wt5iFA3Pn7p6V8M",
+    authDomain: "sistema-lista-de-tarefas.firebaseapp.com",
+    projectId: "sistema-lista-de-tarefas",
+    storageBucket: "sistema-lista-de-tarefas.appspot.com",
+    messagingSenderId: "295743173908",
+    appId: "1:295743173908:web:0ba12623a9bd5650747e01"
+  };
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
+
+
+
+
