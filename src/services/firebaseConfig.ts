d449be175a0e7ce5a8f1b2260d7ff94899e6b07f
mauricioc08 <mauricio-cassiano@hotmail.com>
2024-10-31 20:22:02 -0300

    add next plublic key to project

--- src/services/firebaseConfig.ts
+++ src/services/firebaseConfig.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDWqL5H-IkvAmb9aLx3Wt5iFA3Pn7p6V8M",
-    authDomain: "sistema-lista-de-tarefas.firebaseapp.com",
-    projectId: "sistema-lista-de-tarefas",
-    storageBucket: "sistema-lista-de-tarefas.appspot.com",
-    messagingSenderId: "295743173908",
-    appId: "1:295743173908:web:0ba12623a9bd5650747e01"
-  };
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+};
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
